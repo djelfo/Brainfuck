4d3eeefd391901621fa19edabc9d62b7357422ba
Filippo Ferrando <ferrandodfilippo@gmail.com>
2019-09-30 12:32:21 +0200

    add Brainfuck console log for read output

---
+++ brainfuck.js
@@ -0,0 +1,9 @@
+const bf = require("./interpreter.js");
+
+// js
+
+// Parsing a boilerplate
+console.log(bf.parse(bf.boilerplate.helloWorld));
+
+// The whole output of the parser
+console.log(bf.parse(bf.boilerplate.helloWorld));
